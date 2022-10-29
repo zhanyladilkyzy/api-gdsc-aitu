d5de754fb00a0dac43e988245e88ab905d4142fe
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2022-10-29 18:54:18 +0600

    Initial commit

---
+++ src/main/java/kz/aitu/apigdsc/ApiGdscAituApplication.java
@@ -0,0 +1,13 @@
+package kz.aitu.apigdsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGdscAituApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGdscAituApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/aitu/apigdsc/ApiGdscAituApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aitu.apigdsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGdscAituApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
