991d36c485cc10cce77b41158d480c8fae8debe4
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2022-10-31 08:18:28 +0600

    create User model

--- src/main/java/kz/aitu/apigdsc/model/User.java
+++ src/main/java/kz/aitu/apigdsc/model/User.java
@@ -1,4 +1,26 @@
 package kz.aitu.apigdsc.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private String name;
+
+    private String surname;
+
+    private Long barcode;
+
+    private Long telegramId;
+
+    private String telegramUsername;
+
+    private String phoneNumber;
+
+    private String password;
+
+    private Boolean confirmed;
 }
