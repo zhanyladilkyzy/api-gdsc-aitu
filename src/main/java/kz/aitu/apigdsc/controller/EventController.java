752bf179fa20ff6e220d27d5bb0b329d56ccf1f4
zhanyladilkyzy <52228972+zhanyladilkyzy@users.noreply.github.com>
2022-11-16 23:48:10 +0600

    refactoring

--- src/main/java/kz/aitu/apigdsc/controller/EventController.java
+++ src/main/java/kz/aitu/apigdsc/controller/EventController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/event")
+@RestController
+@RequestMapping("/event")
 public class EventController {
 
     @Autowired
